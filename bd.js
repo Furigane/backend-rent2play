5420c899289d081ac3734977bab39059bba2a99d
Furigane <freedominprogramming@gmail.com>
2025-06-17 20:39:02 +0400

    Backend 2

--- bd.js
+++ bd.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'sql8.freesqldatabase.com',   
-  user: 'sql8785287',          
-  password: 'ZBgIbQHY1T', 
-  database: 'sql8785287',   
+  host: process.env.DB_HOST,   
+  user: process.env.DB_USER,          
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_DATABASE,   
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
